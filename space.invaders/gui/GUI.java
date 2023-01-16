10451ca0f5d4277abd0d409be4bc9b2c61c54046
Wojciech Krzos <276264@student.pwr.edu.pl>
2023-01-16 20:29:51 +0100

    1

--- space.invaders/gui/GUI.java
+++ space.invaders/gui/GUI.java
@@ -9,34 +9,36 @@ import java.awt.event.KeyListener;
 //TODO: przenieść panel do nowej klasy
 
 public class GUI extends JFrame implements KeyListener {
+    MainBodyPanel mbp;
+    
     JPanel mainBodyPanel;
     JLabel player;
+    
     final int player_width = 40;
     final int player_height = 40;
     int x_position = 400;
     final int y_position = 500;
 
-    public GUI(){
+    public GUI() {
+        mbp = new MainBodyPanel();
+
         this.setTitle("Space Invaders");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
-        this.setSize(800,600);
+        this.setSize(800, 600);
 
-//        mainBody
-        mainBodyPanel = new JPanel();
-        mainBodyPanel.setLayout(null);
-        mainBodyPanel.setBackground(Color.BLACK);
-        this.addKeyListener(this);
-        this.add(mainBodyPanel);
+        this.add(mbp.getMainBodyPanel());
+        mbp.getMainBodyPanel().addKeyListener(keyPressed(null));
 
-//        player
+        // player
         Player player1 = new Player(400);
         player = new JLabel("Statek");
-//        player.setSize(40,40);
+
+        // player.setSize(40,40);
         player.setBounds(x_position, y_position, player_width, player_height);
         player.setForeground(Color.RED);
 
-        mainBodyPanel.add(player);
+        mbp.getMainBodyPanel().add(player);
         this.setVisible(true);
     }
 
@@ -52,11 +54,11 @@ public class GUI extends JFrame implements KeyListener {
         int speed = 3;
         boolean moveLeft = keyCode == KeyEvent.VK_LEFT || keyCode == KeyEvent.VK_A;
         boolean moveRight = keyCode == KeyEvent.VK_RIGHT || keyCode == KeyEvent.VK_D;
-        if (moveLeft || moveRight){
-            if (moveLeft){
+        if (moveLeft || moveRight) {
+            if (moveLeft) {
                 System.out.println("Ruch w lewo");
                 x_position -= speed;
-            }else if (moveRight){
+            } else if (moveRight) {
                 System.out.println("Ruch w prawo");
                 x_position += speed;
             }
